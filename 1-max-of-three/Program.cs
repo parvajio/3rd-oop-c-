dc1266696be6eb9e4f2c0287eac972849ff9ef3f
Md. Parvaj Mosharof <mdparvajmosharof17@gmail.com>
2025-02-05 22:58:11 +0600

    max-of-three

---
+++ 1-max-of-three/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+       int a = 23;
+       int b= 34;
+       int c = 234;
+       int max = a;
+       
+       if(b > max ){
+           max = b;
+       }
+       
+       if(c > max){
+           max = c;
+       }
+      
+        Console.WriteLine("The maximum number is: " + max);
+    }
+}
