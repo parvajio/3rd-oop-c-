05a465e0daea02aaf46e7621b20109e5ee32ea71
Md. Parvaj Mosharof <mdparvajmosharof17@gmail.com>
2025-02-05 23:03:34 +0600

    divisible by 3 n 5

---
+++ 2-divisible-by-3n5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class HelloWorld {
+	static void Main() {
+		int num = 2;
+		if(num%3 == 0 && num%5 == 0) {
+			Console.WriteLine($"this {num} is divisible by 3 & 5");
+		}
+		else {
+			if(num%3 == 0) {
+				Console.WriteLine($"this {num} is divisible by 3");
+			}
+			else if (num%5 == 0) {
+                Console.WriteLine($"this {num} is divisible by 5");
+			}
+			else{
+			    Console.WriteLine($"this {num} is not divisible by 3 or  5");
+			}
+		}
+	}
+}
