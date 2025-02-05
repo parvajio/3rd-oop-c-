0c212612cc6f066133c8eedfd95566d87366482f
Md. Parvaj Mosharof <mdparvajmosharof17@gmail.com>
2025-02-05 23:14:30 +0600

    check if char is an alphabet

---
+++ 3-check-if-char-alphabet/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class HelloWorld {
+  static void Main() {
+      char x = 'y';
+      
+      if(char.IsLetter(x)){
+          Console.WriteLine("This is a alphabet.");
+      }
+      else{
+          Console.WriteLine("This is not a alpha");
+      }
+  }
+}
