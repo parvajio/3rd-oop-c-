a641dc2f411ebdeff889d7c4f1ab6a48074859a2
Md. Parvaj Mosharof <mdparvajmosharof17@gmail.com>
2025-02-07 23:02:37 +0600

    array in c#

---
+++ 5-array/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class HelloWorld {
+	static void Main() {
+		double[] dbl = {12.22,23.56,2.46};
+		foreach(double db in dbl) {
+
+			Console.WriteLine(db);
+		}
+		
+		//taking input
+		
+		double[] height = new double[5];
+		
+		for(int i = 0; i<5; i++){
+		    string inputHieght = Console.ReadLine();
+		    height[i] = Convert.ToDouble(inputHieght);
+		    Console.WriteLine(height[i]);
+		}
+	}
+}
