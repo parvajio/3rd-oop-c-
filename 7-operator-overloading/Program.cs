64671738fb289b215e0d17884b8591665d9f5e61
parvajio <mdparvajmosharof17@gmail.com>
2025-05-04 11:19:45 +0600

    + overloading

--- 7-operator-overloading/Program.cs
+++ 7-operator-overloading/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ComplexNumber c1 = new ComplexNumber(2, 4);
+            ComplexNumber c2 = new ComplexNumber(5, 6);
+
+            ComplexNumber c3 = c1 + c2;
+
+            c1.Display();
+            c2.Display();
+            c3.Display();
+        }
+    }
+
+    class ComplexNumber{
+        public int real;
+        public int imaginary;
+        
+        public ComplexNumber(int real, int imaginary){
+            this.real = real;
+            this.imaginary = imaginary;
+        }
+
+        public void Display(){
+            Console.WriteLine($"{real} + {imaginary}i");
+        }
+
+        public static ComplexNumber operator + (ComplexNumber c1, ComplexNumber c2){
+            int newReal = c1.real + c2.real;
+            int newImaginary = c1.imaginary + c2.imaginary;
+
+            ComplexNumber c3 = new ComplexNumber(newReal,newImaginary);
+            return c3;
+        }
+    }
+
+}
