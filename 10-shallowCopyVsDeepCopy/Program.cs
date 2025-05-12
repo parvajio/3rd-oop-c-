83277bc91d28b1e40438136f347a5c4c50d6dd70
MD. Parvaj Mosharof <mdparvajmosharof17@gmail.com>
2025-05-12 05:32:40 +0000

    shallow copy

---
+++ 10-shallowCopyVsDeepCopy/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            A a1 = new A();
+            A a2 = new A();
+            a1 = a2;
+            a1.a= 10;
+            a2.a = 28;
+            Console.WriteLine(a1.a);
+            Console.WriteLine(a2.a);
+        }
+    }
+
+    class A{
+         public int a ;
+        
+    }
+}
